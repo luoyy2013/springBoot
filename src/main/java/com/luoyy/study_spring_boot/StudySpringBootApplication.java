a9795a303b003b3cd06e04a51fe091c6adee25f5
luoyy <luoyy@cotek-robotics.com>
2018-10-25 10:54:03 +0800

    initial commit

---
+++ src/main/java/com/luoyy/study_spring_boot/StudySpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.luoyy.study_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@author luosir
+ */
+@SpringBootApplication
+public class StudySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootApplication.class, args);
+	}
+}
