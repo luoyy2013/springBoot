0089d718e568e8a1e76b7d96f32680e762a5ae85
luoyy <luoyy@cotek-robotics.com>
2018-10-25 14:28:07 +0800

    finished demo url

---
+++ src/main/java/com/luoyy/study_spring_boot/StudySpringBootController.java
@@ -0,0 +1,26 @@
+package com.luoyy.study_spring_boot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>版本: </p>
+ * <p>日期: 18-10-25</p>
+ * <p>作者: LuoYangyang</p>
+ * <pre>描述: </pre>
+ * <pre>版权: luosir</pre>
+ *
+ * @author LuoYangyang
+ */
+@RestController
+public class StudySpringBootController {
+    @Value("${people}")
+    private String people;
+
+
+    @RequestMapping("/hello")
+    public String say(){
+        return people+"ni hao";
+    }
+}
