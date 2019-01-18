64f80c2242e11b8fd57da73b620471be0346ba14
luoyy <luoyy@cotek-robotics.com>
2019-01-18 17:55:05 +0800

    shiro demo

--- src/main/java/com/luoyy/study_spring_boot/StudySpringBootApplication.java
+++ src/main/java/com/luoyy/study_spring_boot/StudySpringBootApplication.java
@@ -3,12 +3,13 @@ package com.luoyy.study_spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  *@author luosir
  */
 @SpringBootApplication
 public class StudySpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringBootApplication.class, args);
 	}
